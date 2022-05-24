5317ac1360a89479be1c9bba089a4ff84d326a3e
Hennie Bruurmijn <hlm.bruurmijn@gmail.com>
2022-05-24 16:53:37 +0200

    Add state after end of tutorial
    Upcoming assignments for future edits:
    - Clean phone numbers
    - Time Targeting
    - Day of the Week Targeting

---
+++ lib/event_manager.rb
@@ -0,0 +1,64 @@
+require 'csv'
+require 'google/apis/civicinfo_v2'
+require 'erb'
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, '0')[0..4]
+end
+
+def legislators_by_zipcode(zipcode)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyARlJtkr0iE0XXRsteh6U3hlu1nX9SGkws'
+  begin
+    civic_info.representative_info_by_address(
+      address: zipcode,
+      levels: 'country',
+      roles: %w[legislatorUpperBody legislatorLowerBody]
+    ).officials
+  rescue StandardError
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+end
+
+def save_thank_you_letter(id, personal_letter)
+  Dir.mkdir('output') unless Dir.exist?('output')
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts personal_letter
+  end
+end
+
+puts 'EventManager Initialized!'
+
+contents = CSV.open(
+  'event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+)
+
+template_letter = File.read('form_letter.erb')
+erb_template = ERB.new template_letter
+
+contents.each do |row|
+  id = row[0]
+  name = row[:first_name]
+  zipcode = clean_zipcode(row[:zipcode])
+  legislators = legislators_by_zipcode(zipcode)
+
+  personal_letter = erb_template.result(binding)
+  save_thank_you_letter(id, personal_letter)
+end
+
+# contents = File.read('event_attendees.csv') if File.exist? "event_attendees.csv"
+# puts contents
+
+# lines = File.readlines('event_attendees.csv')
+# lines.each_with_index do |line, i|
+#   next if i.zero?
+
+#   columns = line.split(',')
+#   name = columns[2]
+#   puts name
+# end
